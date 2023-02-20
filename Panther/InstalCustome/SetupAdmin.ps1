781f4c33120cde2b63a21cea0889869c92fddb19
Tom Laveille <120559974+tlaveille-CENRA@users.noreply.github.com>
2023-02-20 14:12:25 +0100

    Update SetupAdmin.ps1

--- Panther/InstalCustome/SetupAdmin.ps1
+++ Panther/InstalCustome/SetupAdmin.ps1
@@ -4,3 +4,11 @@ $xmlCode =[xml](Get-Content -Path C:\Windows\Panther\unattend.xml -Force)
 $Code = $xmlCode.unattend.settings.component.FirstLogonCommands.SynchronousCommand[2].CommandLine
 if($? -eq $false){$?
 $Code = '4269'}
+
+# Install Module #
+Install-PackageProvider -Name NuGet -Confirm:$false -Force
+if( $? -eq $false){$?}
+Install-Module -Name AudioDeviceCmdlets -Confirm:$false -Force
+if( $? -eq $false){$?}
+Import-Module C:\Windows\Panther\InstalCustome\Set-WindowState.ps1 -Force
+if( $? -eq $false){$?}
