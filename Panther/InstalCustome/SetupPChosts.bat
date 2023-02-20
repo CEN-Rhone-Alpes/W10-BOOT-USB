4a49731bb7093d272409c77a6e579c35e8a6a528
Tom Laveille <120559974+tlaveille-CENRA@users.noreply.github.com>
2023-02-20 14:21:06 +0100

    Update SetupPChosts.bat

--- Panther/InstalCustome/SetupPChosts.bat
+++ Panther/InstalCustome/SetupPChosts.bat
@@ -4,7 +4,7 @@ powershell Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser 
 
 powershell -ExecutionPolicy Bypass -File "C:\Windows\Panther\InstalCustome\Execute.ps1" "/verysilent"
 
-powershell -ExecutionPolicy Bypass "Start-process powershell -Verb runAs C:\Windows\Panther\InstalCustome\FullSetupAdmin.ps1"
+powershell -ExecutionPolicy Bypass "Start-process powershell -Verb runAs C:\Windows\Panther\InstalCustome\SetupAdmin.ps1"
 
 powershell Start-Sleep -Seconds 180
 
@@ -15,5 +15,3 @@ powershell -ExecutionPolicy Bypass -File "C:\Windows\Panther\InstalCustome\Lecte
 powershell -ExecutionPolicy Bypass -File "C:\Windows\Panther\InstalCustome\TrueInstallApp.ps1" "/verysilent"
 
 Pause
-
-
