ce82b5d3fa1f3cb71455c23ff710e24fcc213b25
Tom Laveille <120559974+tlaveille-CENRA@users.noreply.github.com>
2023-02-20 14:12:08 +0100

    Update SetupAdmin.ps1

--- Panther/InstalCustome/SetupAdmin.ps1
+++ Panther/InstalCustome/SetupAdmin.ps1
@@ -1,1 +1,6 @@
 
+# Choix Geo #
+$xmlCode =[xml](Get-Content -Path C:\Windows\Panther\unattend.xml -Force)
+$Code = $xmlCode.unattend.settings.component.FirstLogonCommands.SynchronousCommand[2].CommandLine
+if($? -eq $false){$?
+$Code = '4269'}
